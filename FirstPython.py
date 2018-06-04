1e742e9df6ad5d8a6554b797a6ecefac17d367d7
ThomasManalil <thomuuuu@gmail.com>
2018-06-05 03:15:48 +0530

    modified sum function to sumOfTwo and added sum_array function

--- FirstPython.py
+++ FirstPython.py
@@ -1,13 +1,19 @@
 
-def sum(a,b):
+def sumOfTwo(a,b):
     return a+b
+
+def sum_array(arr):
+    return sum(arr)
+
 
 
 def main():
     print ("Hello World")
-    s=sum(1,2)
+    s=sumOfTwo(1,2)
     print ("Sum is:",s)
-
+    array = [1,2,3,4]
+    sumOfArray = sum_array (array)
+    print (sumOfArray)
 
 if __name__ == '__main__':
     main()
